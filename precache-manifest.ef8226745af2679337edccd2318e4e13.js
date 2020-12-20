bbc9c24db46baf7c0335a215c7853c63035ca7e9
Faruk <felixfranko66@gmail.com>
2020-12-20 04:05:09 +0300

    Updates

---
+++ precache-manifest.ef8226745af2679337edccd2318e4e13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d111e0a2671bea3daa6388cf26f4a99c",
+    "url": "/react-webpage-1/index.html"
+  },
+  {
+    "revision": "b53515f84ae178067827",
+    "url": "/react-webpage-1/static/css/main.43db99ef.chunk.css"
+  },
+  {
+    "revision": "327422916521199c04b8",
+    "url": "/react-webpage-1/static/js/2.e76f23f3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-webpage-1/static/js/2.e76f23f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53515f84ae178067827",
+    "url": "/react-webpage-1/static/js/main.f54c4a5b.chunk.js"
+  },
+  {
+    "revision": "01ab106ad205a93dddcc",
+    "url": "/react-webpage-1/static/js/runtime-main.890a4765.js"
+  }
+]);
