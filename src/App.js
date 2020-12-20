0fc063ac379ea6f0b8e690d8e5037c46984c7ab4
Faruk <felixfranko66@gmail.com>
2020-12-20 03:10:53 +0300

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path='/' component={Home} />
+          <Route path='/services' exact component={Services} />
+          <Route path='/products' exact component={Products} />
+          <Route path='/sign-up' exact component={SignUp} />
         </Switch>
         <Footer />
       </Router>
